5fd052d51befc23643cbc815cfdaedc5262217e1
adam <adam@monkeez.org>
2020-05-23 19:00:44 +0100

    Now removes a row and adds a clean one if it is complete

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const width = 10
   let nextRandom = 0
   let timerId
+  let score = 0
 
   // The tetriminoes
   const lTetromino = [
@@ -102,6 +103,7 @@ function freeze(){
     currentPosition = 4
     draw()
     displayShape()
+    addScore()
   }
 }
 
@@ -181,6 +183,24 @@ startBtn.addEventListener('click', () => {
   }
 })
 
+// add score
+function addScore(){
+  for (let i = 0; i < 199; i += width){
+    const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
 
+    if(row.every(index => squares[index].classList.contains('taken'))) {
+      score +=10
+      scoreDisplay.innerHTML = score
+      row.forEach(index => {
+        squares[index].classList.remove('taken')
+        squares[index].classList.remove('tetrimino')
+        squares[index].style.backgroundColor = ''
+      })
+      const squaresRemoved = squares.splice(i, width)
+      squares = squaresRemoved.concat(squares)
+      squares.forEach(cell => grid.appendChild(cell))
+    }
+  }
+}
 
 })
