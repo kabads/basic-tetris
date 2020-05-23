126e97cc3e0e7b3b6ee37bb05d1376be6a9450f4
adam <adam@monkeez.org>
2020-05-23 16:37:39 +0100

    We can now pause and start with the button

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   let squares = Array.from(document.querySelectorAll('.grid div'))
-  const ScoreDisplay = document.querySelector('#score')
-  const StartBtn = document.querySelector('#start-button')
+  const scoreDisplay = document.querySelector('#score')
+  const startBtn = document.querySelector('#start-button')
   const width = 10
+  let nextRandom = 0
+  let timerId
 
   // The tetriminoes
-  const lTetrimino = [
+  const lTetromino = [
     [1, width+1, width*2+1, 2],
     [width, width+1, width+2, width*2+2],
     [1, width+1, width*2+1, width*2+2],
@@ -40,7 +42,7 @@ document.addEventListener('DOMContentLoaded', () => {
      [1,width+1,width*2+1,width*3+1],
      [width,width+1,width+2,width+3]
    ]
-  const theTetriminoes = [lTetrimino, zTetromino, tTetromino, oTetromino, iTetromino]
+  const theTetriminoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
 
   let currentPosition = 4
   let currentRotation = 0
@@ -65,21 +67,20 @@ function undraw(){
 }
 
 // make the tetrimino move down every second
-timerId = setInterval(moveDown, 500) // This number is set to 500 for debugging to make the tetriminoes fall faster
+// timerId = setInterval(moveDown, 500) // This number is set to 500 for debugging to make the tetriminoes fall faster
 
 // assign functions to keycodes
 function control(e){
   if(e.keyCode === 37){
   moveLeft()
   } else if (e.keyCode === 38){
-    // rotate()
+    rotate()
   } else if (e.keyCode === 39){
     moveRight()
   } else if (e.keyCode === 40) {
     moveDown()
   }
 }
-
 document.addEventListener('keyup', control)
 
 //moveDown function
@@ -95,10 +96,12 @@ function freeze(){
   if (current.some(index => squares[currentPosition + index + width].classList.contains('taken'))){
     current.forEach(index=> squares[currentPosition + index].classList.add('taken'))
     //start a new tetrimino
-    random = Math.floor(Math.random() * theTetriminoes.length)
+    random = nextRandom
+    nextRandom = Math.floor(Math.random() * theTetriminoes.length)
     current = theTetriminoes[random][currentRotation]
     currentPosition = 4
     draw()
+    displayShape()
   }
 }
 
@@ -125,6 +128,59 @@ function moveRight() {
   }
   draw()
 }
+
+// rotate the tetriminoes
+function rotate(){
+  undraw()
+  currentRotation ++
+  if (currentRotation  === current.length){ // if the current rotation gets to 4 make it go back to 0
+    currentRotation = 0
+  }
+  current = theTetriminoes[random][currentRotation]
+  draw()
+}
+
+// show up-next tetrimino in mini-grid
+const displaySquares = document.querySelectorAll('.mini-grid div')
+const displayWidth = 4
+let displayIndex = 0
+
+
+// the tetriminoes without rotations
+const upNextTetriminoes = [
+  [1, displayWidth+1, displayWidth*2+1,2], // LTetrimino
+  [0, displayWidth, displayWidth+1, displayWidth*2+1], // z tetrimino
+  [1, displayWidth, displayWidth+1, displayWidth+2], // t tetrimino
+  [0, 1, displayWidth, displayWidth+1], // o tetrimino
+  [1, displayWidth+1, displayWidth*2+1, displayWidth*3+1] // i tetrimino
+]
+
+// display the shape in the mini grid
+
+function displayShape() {
+  // remove any trace af a tetrimino from the entire grid
+  displaySquares.forEach(square => {
+    square.classList.remove('tetrimino')
+  })
+upNextTetriminoes[nextRandom].forEach(index => {
+  displaySquares[displayIndex + index].classList.add('tetrimino')
+})
+}
+
+// add functionality to the button
+startBtn.addEventListener('click', () => {
+  if(timerId){
+    clearInterval(timerId)
+    timerId = null
+  } else {
+    draw()
+    timerId = setInterval(moveDown, 1000)
+    nextRandom = Math.floor(Math.random()*theTetriminoes.length)
+    displayShape()
+
+  }
+})
+
 
 
 })
